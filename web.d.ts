55eb1b12c71ad52d93f059a5dab669ae05ffb185
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-02-28 07:33:05 +0000

    Deploying to gh-pages from @ b-on-g/brl@d8af73a9cb2d9e0767aff357fc37830d97014c47 🚀

--- web.d.ts
+++ web.d.ts
@@ -777,7 +777,7 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    let $mol_gap: Record<"text" | "block" | "space" | "page" | "blur" | "round", $mol_style_func<"var", unknown>>;
+    let $mol_gap: Record<"text" | "block" | "space" | "page" | "blur" | "round" | "emoji", $mol_style_func<"var", unknown>>;
 }
 
 declare namespace $ {

39d06c9488124c770db55a60be1d94902099a419
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-03-17 07:57:44 +0000

    Deploying to gh-pages from @ b-on-g/brl@d8af73a9cb2d9e0767aff357fc37830d97014c47 🚀

--- node.js
+++ node.js
@@ -6119,6 +6119,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
--- web.mjs
+++ web.mjs
@@ -4773,6 +4773,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
